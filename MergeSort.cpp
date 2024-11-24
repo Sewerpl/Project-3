9e0b626e8cc9f60c9c1a6df9fb3f907bcb9398fb
PC <seweryn.contact@gmail.com>
2024-11-24 21:44:07 +0100

    scalanie dwóch posortowanych części tablicy

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -16,3 +16,33 @@ void MergeSort::mergeSort(std::vector<int>& array, int left, int right) {
         merge(array, left, mid, right);
     }
 }
+
+void MergeSort::merge(std::vector<int>& array, int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    std::vector<int> leftArray(n1);
+    std::vector<int> rightArray(n2);
+
+    for (int i = 0; i < n1; ++i)
+        leftArray[i] = array[left + i];
+    for (int i = 0; i < n2; ++i)
+        rightArray[i] = array[mid + 1 + i];
+
+    int i = 0, j = 0, k = left;
+    while (i < n1 && j < n2) {
+        if (leftArray[i] <= rightArray[j]) {
+            array[k++] = leftArray[i++];
+        } else {
+            array[k++] = rightArray[j++];
+        }
+    }
+
+    while (i < n1) {
+        array[k++] = leftArray[i++];
+    }
+
+    while (j < n2) {
+        array[k++] = rightArray[j++];
+    }
+}
