9ce97c30bc273d172881dd4b00e0dbfefc6b5f49
PC <seweryn.contact@gmail.com>
2024-11-24 21:40:12 +0100

    Rekurencyjne dzielenie wektora na mniejsze podtablice

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -5,3 +5,14 @@ void MergeSort::sort(std::vector<int>& array) {
         mergeSort(array, 0, array.size() - 1);
     }
 }
+
+void MergeSort::mergeSort(std::vector<int>& array, int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+
+        mergeSort(array, left, mid);
+        mergeSort(array, mid + 1, right);
+
+        merge(array, left, mid, right);
+    }
+}
