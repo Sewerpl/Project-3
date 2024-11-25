5a4495caf88171f43265d060812ecd39c668b10b
PC <seweryn.contact@gmail.com>
2024-11-25 11:03:55 +0100

    Sprawdzenie losowego sortowania

--- MergeSortTest.cpp
+++ MergeSortTest.cpp
@@ -16,3 +16,11 @@ TEST(MergeSortTest, ReverseOrder) {
     MergeSort::sort(array);
     EXPECT_EQ(array, expected);
 }
+
+TEST(MergeSortTest, RandomArray) {
+    std::vector<int> array = {4, 1, 3, 9, 7};
+    std::vector<int> expected = {1, 3, 4, 7, 9};
+
+    MergeSort::sort(array);
+    EXPECT_EQ(array, expected);
+}
